b58a951cf9eff06f205dc197ce90157b688a70d7
Daniel Desira <desiradaniel2007@gmail.com>
2016-03-16 11:27:22 +0100

    refeeded from J&A bonnici theme

--- includes/contact-widget.php
+++ includes/contact-widget.php
@@ -21,18 +21,39 @@ class ADZ_Contact_Widget extends WP_Widget {
 	 * @param instance Widget settings.
 	 */
 	public function widget( $args, $instance ) {
+		$address_line_1 = $instance['address_line_1'];
+		$address_line_2 = $instance['address_line_2'];
+		$address_line_3 = $instance['address_line_3'];
+		$address_line_4 = $instance['address_line_4'];
 		$phone = $instance['phone'];
 		$email = $instance['email'];
 		
 		?>
 		<h4>Contact Us</h4>
+		<?php if ( ! empty( $address_line_1 ) ||
+				! empty( $address_line_2 ) ||
+				! empty( $address_line_3 ) ||
+				! empty( $address_line_4 ) ): ?>
+		<p>
+			<i class="fa fa-map-marker"></i>
+			<?php echo esc_attr( $address_line_1 ); ?>,<br>
+			<?php echo esc_attr( $address_line_2 ); ?>,<br>
+			<?php echo esc_attr( $address_line_3 ); ?>,<br>
+			<?php echo esc_attr( $address_line_4 ); ?>.
+		</p>
+		<?php endif;
+		
+		if ( ! empty( $phone ) ): ?>
 		<p>
 			<a href="tel:+356<?php echo esc_attr( $phone ); ?>"><i class="fa fa-phone"></i> +356 <?php echo esc_attr( $phone ); ?></a>
 		</p>
+		<?php endif;
+		
+		if ( ! empty( $email ) ): ?>
 		<p>
 			<a href="mailto:<?php echo esc_attr( $email ); ?>"><i class="fa fa-envelope"></i> <?php echo esc_attr( $email ); ?></a>
 		</p>
-		<?php
+		<?php endif;
 		
 	}
 	
@@ -41,10 +62,26 @@ class ADZ_Contact_Widget extends WP_Widget {
 	 * @param instance Widget settings.
 	 */
 	public function form( $instance ) {
+		$address_line_1 = $instance['address_line_1'];
+		$address_line_2 = $instance['address_line_2'];
+		$address_line_3 = $instance['address_line_3'];
+		$address_line_4 = $instance['address_line_4'];
 		$phone = $instance['phone'];
 		$email = $instance['email'];
 		
 		?>
+		<label for="<?php echo $this->get_field_id( 'address_line_1' ); ?>">Address Line 1</label>
+		<input type="text" id="<?php echo $this->get_field_id( 'address_line_1' ); ?>" name="<?php echo $this->get_field_name( 'address_line_1' ); ?>"
+			value="<?php echo esc_attr( $address_line_1 ); ?>"><br>
+		<label for="<?php echo $this->get_field_id( 'address_line_2' ); ?>">Address Line 2</label>
+		<input type="text" id="<?php echo $this->get_field_id( 'address_line_2' ); ?>" name="<?php echo $this->get_field_name( 'address_line_2' ); ?>"
+			value="<?php echo esc_attr( $address_line_2 ); ?>"><br>
+		<label for="<?php echo $this->get_field_id( 'address_line_3' ); ?>">Address Line 3</label>
+		<input type="text" id="<?php echo $this->get_field_id( 'address_line_3' ); ?>" name="<?php echo $this->get_field_name( 'address_line_3' ); ?>"
+			value="<?php echo esc_attr( $address_line_3 ); ?>"><br>
+		<label for="<?php echo $this->get_field_id( 'address_line_4' ); ?>">Address Line 4</label>
+		<input type="text" id="<?php echo $this->get_field_id( 'address_line_4' ); ?>" name="<?php echo $this->get_field_name( 'address_line_4' ); ?>"
+			value="<?php echo esc_attr( $address_line_4 ); ?>"><br>
 		<label for="<?php echo $this->get_field_id( 'phone' ); ?>">Phone</label>
 		<input type="number" id="<?php echo $this->get_field_id( 'phone' ); ?>" name="<?php echo $this->get_field_name( 'phone' ); ?>"
 			value="<?php echo esc_attr( $phone ); ?>"><br>
