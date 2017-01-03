8259119c64ee4db5f25f9933cda1c76a057b5578
Daniel <desiradaniel2007@hotmail.com>
2017-01-03 18:00:37 +0100

    sidebar looks better

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <div id="sidebar" class="sidebar">
+	<button id="sidebar-toggle" class="btn btn-default sidebar-toggle" type="button">Toggle sidebar</button>
 <?php if ( is_active_sidebar( 'sidebar' ) ): ?>
 	<div class="widget-area" role="complementary">
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</div>
 <?php endif; ?>
-	<button id="sidebar-toggle" class="btn btn-default sidebar-toggle" type="button">Toggle sidebar</button>
 </div>
