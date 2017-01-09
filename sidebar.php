7140cdfa6ae7c583aeb975a61957d8def06bf5a5
Daniel <desiradaniel2007@hotmail.com>
2017-01-09 11:02:46 +0100

    cleaned up file

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,18 @@
 <div id="sidebar" class="sidebar">
 	<button id="sidebar-lock" class="btn btn-default sidebar-lock" type="button">Lock sidebar</button>
 	<div class="widget-area" role="complementary">
-	<?php if ( is_active_sidebar( 'sidebar' ) ): ?>
-	
-		<?php dynamic_sidebar( 'sidebar' ); ?>
-	
-	<?php endif; ?>
 	<?php
+	
+	if ( is_active_sidebar( 'sidebar' ) ) {
+		dynamic_sidebar( 'sidebar' );
+	}
+	
+	wp_list_bookmarks( array(
+		'title_li' => 'Blogroll',
+		'title_before' => '<h4>',
+		'title_after' => '</h4>',
+	) );
 		
-		wp_list_bookmarks( array(
-			'title_li' => 'Blogroll',
-			'title_before' => '<h4>',
-			'title_after' => '</h4>',
-		) );
-		
-		?>
+	?>
 	</div>
 </div>
