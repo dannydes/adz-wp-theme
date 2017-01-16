45699f76eb6e6710d2c0f81088c966b81f8241c9
Daniel <desiradaniel2007@hotmail.com>
2017-01-16 16:52:34 +0100

    connected to mailchimp and bootstrap style

--- includes/widgets/mailchimp-subscribe-widget.php
+++ includes/widgets/mailchimp-subscribe-widget.php
@@ -27,15 +27,27 @@ class ADZ_Mailchimp_Subscribe_Widget extends WP_Widget {
 		
 		?>
 		<h4>Subscribe to our newsletter</h4>
-		<form>
-			<label for="mailchimp-name">Name</label>
-			<input type="text" id="mailchimp-name">
-			<label for="mailchimp-surname">Surname</label>
-			<input type="text" id="mailchimp-surname">
-			<label for="mailchimp-email">Email</label>
-			<input type="email" id="mailchimp-email">
-			<input type="submit" value="Subscribe">
+		<form action="//wordpress.us14.list-manage.com/subscribe/post?u=339dfe0090694d284c3a800bf&amp;id=d2c9c3a5c0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<div class="form-group">
+				<label for="mailchimp-name">Name</label>
+				<input type="text" id="mailchimp-name" name="FNAME" class="form-control" placeholder="Name">
+			</div>
+			<div class="form-group">
+				<label for="mailchimp-surname">Surname</label>
+				<input type="text" id="mailchimp-surname" name="LNAME" class="form-control" placeholder="Surname">
+			</div>
+			<div class="form-group">
+				<label for="mailchimp-email">Email</label>
+				<input type="email" id="mailchimp-email" name="EMAIL" class="form-control" placeholder="Email">
+			</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_339dfe0090694d284c3a800bf_d2c9c3a5c0" tabindex="-1" value=""></div>
+			<input type="submit" class="btn btn-default" value="Subscribe">
 		</form>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 		<?php
 		
 	}
