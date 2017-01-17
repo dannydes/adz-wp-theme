9fd5f9fefd73a6e1a723c28136177e94fba6a5e5
Daniel <desiradaniel2007@hotmail.com>
2017-01-17 18:01:52 +0100

    pagination gets most of Bootstrap's UI

--- index.php
+++ index.php
@@ -8,12 +8,21 @@ get_sidebar();
 <div class="page-header">
 	<h1><?php single_post_title(); ?></h1>
 </div>
-<?php if ( have_posts() ): ?>
+<?php
+
+$paged = ( get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1 );
+
+$the_query = new WP_Query( array(
+	'posts_per_page' => 10,
+	'paged' => $paged,
+) );
+
+if ( $the_query->have_posts() ): ?>
 <div class="row">
 <?php
 
-	while ( have_posts() ) {
-		the_post();
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
 		get_template_part( 'includes/blog/post-excerpt-part' );
 	}
 
@@ -21,9 +30,24 @@ get_sidebar();
 </div>
 <?php
 
-	the_posts_pagination( array(
-		'mid_size' => 2,
+	$big = 999999999;
+	
+?>
+<nav aria-label="Page navigation">
+  <ul class="pagination">
+	<?php
+	
+	echo paginate_links( array(
+		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'current' => $paged,
+		'total' => $the_query->max_num_pages,
+		'type' => 'list',
 	) );
+	
+	?>
+	</ul>
+</nav>
+<?php
 
 else:
 	get_template_part( 'includes/blog/no-posts' );
