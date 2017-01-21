2f2dd3b267282956b48d92b9570e3c3ff1fb1e0c
Daniel <desiradaniel2007@hotmail.com>
2017-01-21 19:28:20 +0100

    call-to-action block design

--- front-page.php
+++ front-page.php
@@ -8,11 +8,22 @@ the_post();
 
 the_content();
 
+?>
+</div>
+<div class="call-to-action-block">
+	<div class="container">
+		<p>endgfsl.sdfl</p>
+		<a href="#" role="button" class="btn btn-success">Join us</a>
+	</div>
+</div>
+<div class="container">
+<?php
+
 $query = new WP_Query( 'posts_per_page=5' );
 
 if ( $query->have_posts() ): ?>
-<h2>Recent News</h2>
-<div class="row">
+	<h2>Recent News</h2>
+	<div class="row">
 <?php
 
 	while ( $query->have_posts() ) {
@@ -21,7 +32,7 @@ if ( $query->have_posts() ): ?>
 	}
 
 ?>
-</div>
+	</div>
 <?php
 
 endif;
