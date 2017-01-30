23f565a288cf904459ec5c10b74b6f79f4cd2036
Daniel <desiradaniel2007@hotmail.com>
2017-01-30 13:30:57 +0100

    fixed issues new pagination caused to archive

--- archive.php
+++ archive.php
@@ -9,25 +9,8 @@ get_sidebar();
 	<h1><?php the_archive_title(); ?></h1>
 	<p><?php the_archive_description(); ?></p>
 </div>
-<?php if ( have_posts() ): ?>
-<div class="row">
 <?php
 
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'includes/post-excerpt-part' );
-	}
-
-?>
-</div>
-<?php
-
-	the_posts_pagination( array(
-		'mid_size' => 2,
-	) );
-
-else:
-	get_template_part( 'includes/no-posts' );
-endif;
+get_template_part( 'includes/blog/posts-list' );
 
 get_footer();
---
+++ includes/blog/posts-list.php
@@ -0,0 +1,44 @@
+<?php
+
+$paged = ( get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1 );
+
+$the_query = new WP_Query( array(
+	'posts_per_page' => 10,
+	'paged' => $paged,
+) );
+
+if ( $the_query->have_posts() ): ?>
+<div class="row">
+<?php
+
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		get_template_part( 'includes/blog/post-excerpt-part' );
+	}
+
+?>
+</div>
+<?php
+
+	$big = 999999999;
+	
+?>
+<nav aria-label="Page navigation">
+  <ul class="pagination">
+	<?php
+	
+	echo paginate_links( array(
+		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'current' => $paged,
+		'total' => $the_query->max_num_pages,
+		'type' => 'list',
+	) );
+	
+	?>
+	</ul>
+</nav>
+<?php
+
+else:
+	get_template_part( 'includes/blog/no-posts' );
+endif;
