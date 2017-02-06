3463abdca5fc542457633e07d87be42eb60c8748
Daniel <desiradaniel2007@hotmail.com>
2017-02-06 14:56:07 +0100

    TRUE -> true

--- includes/admin.php
+++ includes/admin.php
@@ -4,7 +4,7 @@
  * Loads backend scripts.
  */
 function ecologie_admin_enqueue_scripts() {
-	wp_enqueue_script( 'admin-base-js', get_template_directory_uri() . '/js/admin-script.js', 'jquery', wp_get_theme()->get( 'Version' ), TRUE );
+	wp_enqueue_script( 'admin-base-js', get_template_directory_uri() . '/js/admin-script.js', 'jquery', wp_get_theme()->get( 'Version' ), true );
 }
 
 add_action( 'admin_enqueue_scripts', 'ecologie_admin_enqueue_scripts' );
