0a542454f1710bd4e6857347ab9de2e9eb296274
Daniel <desiradaniel2007@hotmail.com>
2017-07-24 22:04:30 +0200

    Re-introduced direct Form ID and User ID optional update

--- includes/widgets/mailchimp-subscribe-widget.php
+++ includes/widgets/mailchimp-subscribe-widget.php
@@ -63,7 +63,8 @@ class Ecologie_Mailchimp_Subscribe_Widget extends WP_Widget {
 		?>
 		<label for="<?php echo $this->get_field_id( 'html' ); ?>">Original Form HTML</label>
 		<p>Enter your form's generated HTML to extract User and Form IDs.</p>
-		<textarea id="<?php echo $this->get_field_id( 'html' ); ?>" name="<?php echo $this->get_field_name( 'html' ); ?>" cols="60" rows="20"><?php echo esc_attr( $instance['html'] ); ?></textarea><br>
+		<textarea id="<?php echo $this->get_field_id( 'html' ); ?>" name="<?php echo $this->get_field_name( 'html' ); ?>" cols="60" rows="20"><?php echo esc_attr( $instance['html'] ); ?></textarea>
+		<p>Or if you know what what you're doing, enter your User and Form IDs in the fields below.</p>
 		<label for="<?php echo $this->get_field_id( 'user_id' ); ?>">User ID</label>
 		<input type="text" id="<?php echo $this->get_field_id( 'user_id' ); ?>" name="<?php echo $this->get_field_name( 'user_id' ); ?>" value="<?php echo esc_attr( $instance['user_id'] ); ?>"><br>
 		<label for="<?php echo $this->get_field_id( 'form_id' ); ?>">Form ID</label>
@@ -92,6 +93,9 @@ class Ecologie_Mailchimp_Subscribe_Widget extends WP_Widget {
 			$instance['user_id'] = strip_tags( substr( $action, strpos( $action, 'u=' ) + 2, strpos( $action, '&' ) ) );
 			$instance['form_id'] = strip_tags( substr( $action, strpos( $action, 'id=' ) + 3 ) );
 		}
+		
+		$instance['user_id'] = ( ! empty( $new_instance['user_id'] ) ? strip_tags( $new_instance['user_id'] ) : '' );
+		$instance['form_id'] = ( ! empty( $new_instance['form_id'] ) ? strip_tags( $new_instance['form_id'] ) : '' );
 		
 		return $instance;
 	}
