7720325f9c8a3ecf16a38b4b5284dbf475237e13
Daniel <desiradaniel2007@hotmail.com>
2017-07-29 08:12:12 +0200

    removed checks for empty fields given they are required through markup

--- js/contact.js
+++ js/contact.js
@@ -6,29 +6,25 @@
 			return false;
 		}
 		
-		if ( $( '#contact-name' ).val() !== '' && $( '#contact-email' ).val() !== '' && $( '#contact-message' ).val() !== '' ) {
-			$.post( ecologie.ajax_url, {
-				'action': 'contact_us',
-				'name': $( '#contact-name' ).val(),
-				'email': $( '#contact-email' ).val(),
-				'subject': $( '#contact-subject' ).val(),
-				'message': $( '#contact-message' ).val(),
-				'forward-copy': $( '#contact-copy' ).is( ':checked' ),
-				'at': $( '#contact-at' ).val(),
-			}, function ( res ) {
-				var lines = res.split( '\n' );
-				var status = lines[lines.length - 1];
-				
-				if ( status === '1' ) {
-					Ecologie.createAlert( '#contact-us', 'success', 'Email sent successfully!' );
-					$( '#contact-us input, #contact-message' ).val( '' );
-				} else {
-					Ecologie.createAlert( '#contact-us', 'warning', status );
-				}
-			} );
-		} else {
-			Ecologie.createAlert( '#contact-us', 'warning', 'Please provide your name, your email and your message.' );
-		}
+		$.post( ecologie.ajax_url, {
+			'action': 'contact_us',
+			'name': $( '#contact-name' ).val(),
+			'email': $( '#contact-email' ).val(),
+			'subject': $( '#contact-subject' ).val(),
+			'message': $( '#contact-message' ).val(),
+			'forward-copy': $( '#contact-copy' ).is( ':checked' ),
+			'at': $( '#contact-at' ).val(),
+		}, function ( res ) {
+			var lines = res.split( '\n' );
+			var status = lines[lines.length - 1];
+			
+			if ( status === '1' ) {
+				Ecologie.createAlert( '#contact-us', 'success', 'Email sent successfully!' );
+				$( '#contact-us input, #contact-message' ).val( '' );
+			} else {
+				Ecologie.createAlert( '#contact-us', 'warning', status );
+			}
+		} );
 		
 		return false;
 	});
