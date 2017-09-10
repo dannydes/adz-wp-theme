e25cbe62ad7d4f52dfdf476ea09d2c33d7133c71
Daniel <desiradaniel2007@hotmail.com>
2017-09-10 15:34:51 +0200

    copied some code for label extraction for the sake of issue #46

--- includes/contact.php
+++ includes/contact.php
@@ -189,9 +189,21 @@ function ecologie_google_auth() {
 		$token = $client->fetchAccessTokenWithAuthCode( $_GET['code'] );
 	}
 	
+	$service = new Google_Service_Gmail( $client );
+	$user = 'me';
+$results = $service->users_labels->listUsersLabels($user);
+
+	if (count($results->getLabels()) == 0) {
+		print "No labels found.\n";
+	} else {
+		print "Labels:\n";
+		foreach ($results->getLabels() as $label) {
+			printf("- %s\n", $label->getName());
+		}
+	}
 }
 
-add_action( 'init', 'ecologie_google_auth' );
+add_action( 'admin_init', 'ecologie_google_auth' );
 
 /**
  * Generates a random arithmetic CAPTCHA.
