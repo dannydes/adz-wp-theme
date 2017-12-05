1f8c8eeac84da125961354eba437301fc8980b25
Daniel Desira <desiradaniel2007@hotmail.com>
2017-12-05 18:26:08 +0100

    untested auth method

--- includes/gmail-auth.php
+++ includes/gmail-auth.php
@@ -12,23 +12,26 @@ function ecologie_get_google_client_id() {
 }
 
 /**
- * Authenticates with Google.
+ * Prepare Google client to get SMTP details.
  *
  * @since 0.9
  *
+ * @return The Google client.
  */
-function ecologie_google_auth() {
+function ecologie_google_client() {
 	$client = new Google_Client();
 	$client->setClientId( ecologie_get_google_client_id() );
 	$client->setClientSecret( ecologie_get_theme_mod_or_default( 'contact_sc_gapi_client_secret' ) );
-	$client->addScope( Google_Service_Gmail::GMAIL_SEND );
+	$client->setScopes( 'https://mail.google.com/' );
 	$client->setRedirectUri( admin_url( 'customize.php?action=google_auth_grant' ) );
+	$client->setAccessType( 'offline' );
 	if ( isset( $_GET['code'] ) ) {
 		$token = $client->fetchAccessTokenWithAuthCode( $_GET['code'] );
 	}
 	
-	$service = new Google_Service_Gmail( $client );
-	$user = 'me';
+	return $client;
+	//$service = new Google_Service_Gmail( $client );
+	//$user = 'me';
 	/*$results = $service->users_labels->listUsersLabels($user);
 
 	if (count($results->getLabels()) == 0) {
@@ -39,6 +42,34 @@ function ecologie_google_auth() {
 			printf("- %s\n", $label->getName());
 		}
 	}*/
+}
+
+/**
+ * Authenticates to Google.
+ *
+ * @since 0.9
+ * @uses ecologie_google_client()
+ *
+ * @return Google access token.
+ */
+function ecologie_google_auth() {
+	$client = ecologie_google_client();
+	$access_token = get_theme_mod( 'contact_sc_gapi_access_token' );
+	if ( ! $access_token ) {
+		return array( 'authorization_uri' => $client->createAuthUrl() );
+	}
+	
+	$client->setAccessToken( $access_token );
+	
+	// Refresh token, if expired.
+	if ( $client->isAccessTokenExpired() ) {
+		$client->refreshToken( $client->getRefreshToken() );
+		$new_access_token = $client->getAccessToken();
+		set_theme_mod( 'contact_sc_gapi_access_token', json_encode( $new_access_token ) );
+		return json_decode( $new_access_token, true );
+	}
+	
+	return json_decode( $access_token, true );
 }
 
 // In case admin wants to use Google API, authenticate with it.
