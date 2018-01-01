714acbe035d5e40addfcc4dd089cff98efc1c8b8
Daniel Desira <desiradaniel2007@hotmail.com>
2018-01-01 11:18:12 +0100

    Mailchimp Subscribe Widget: Fixed regression with interaction

--- changelog.md
+++ changelog.md
@@ -1,5 +1,6 @@
 # 0.9.1 ([Release date])
 
+* Mailchimp Subscribe Widget: Fixed regression with interaction
 * Contact Widget: Country codes now fetched through restcountries.eu
 * Contact Widget: Calling codes now visible in dropdown options
 * Navigation Menus: No bullets/separators if no menu is yet set for location
--- js/mailchimp.js
+++ js/mailchimp.js
@@ -1,11 +1,1 @@
 (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));
-(function ( $ ) {
-	'use strict';
-	
-	$( '#mc-embedded-subscribe-form' ).submit(function () {console.log('hello');
-		if ( $( '#mailchimp-name' ).val() === '' || $( '#mailchimp-surname' ).val() === '' || $( '#mailchimp-email' ).val() === '' ) {
-			$( this ).append( '<p>Please fill in all fields!</p>' );
-			return false;
-		}
-	});
-})( jQuery );
