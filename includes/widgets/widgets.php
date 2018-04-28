cf0e78bb9505167af3d739297b5bb21c96a9c53d
Daniel Desira <desiradaniel2007@hotmail.com>
2018-04-28 17:04:38 +0200

    localisable widgets

--- includes/widgets/widgets.php
+++ includes/widgets/widgets.php
@@ -21,7 +21,7 @@ function ecologie_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Footer Column 1', 'ecologie_footer_col_1' ),
 		'id' => 'footer-col-1',
-		'description' => '1st footer column.',
+		'description' => __( '1st footer column.', 'ecologie' ),
 		'before_widget' => '',
 		'after_widget' => '',
 	) );
@@ -29,7 +29,7 @@ function ecologie_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Footer Column 2', 'ecologie_footer_col_2' ),
 		'id' => 'footer-col-2',
-		'description' => '2nd footer column.',
+		'description' => __( '2nd footer column.', 'ecologie' ),
 		'before_widget' => '',
 		'after_widget' => '',
 	) );
@@ -37,7 +37,7 @@ function ecologie_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Footer Column 3', 'ecologie_footer_col_3' ),
 		'id' => 'footer-col-3',
-		'description' => '3rd footer column.',
+		'description' => __( '3rd footer column.', 'ecologie' ),
 		'before_widget' => '',
 		'after_widget' => '',
 	) );
@@ -45,7 +45,7 @@ function ecologie_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Footer Column 4', 'ecologie_footer_col_4' ),
 		'id' => 'footer-col-4',
-		'description' => '4th footer column.',
+		'description' => __( '4th footer column.', 'ecologie' ),
 		'before_widget' => '',
 		'after_widget' => '',
 	) );
@@ -53,7 +53,7 @@ function ecologie_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Sidebar', 'ecologie_sidebar' ),
 		'id' => 'sidebar',
-		'description' => 'Sidebar.',
+		'description' => __( 'Sidebar.', 'ecologie' ),
 		'before_widget' => '',
 		'after_widget' => '',
 	) );
